58e4feb765ec43d07b6da7d88d0bed79b920358e
Gustavo Fonseca <gustavofons@gmail.com>
2011-12-22 18:05:35 -0200

    migrating gh-pages

--- scielobooks/static/js/evaluation-books_list.js
+++
@@ -1,4 +0,0 @@
-$(document).ready(function(){
-    /* start tablesorter */
-    $("#evaluation-table").tablesorter();
-});
--- scielobooks/static/js/remove_none.js
+++
@@ -1,9 +0,0 @@
-$(document).ready(function(){
-
-    var inputText = $("[type='text']");
-
-    for (var i=0; i < inputText.length; i++)
-        if (inputText[i].value == 'None')
-            inputText[i].value = ''
-
-});
